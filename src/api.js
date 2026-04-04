25db3e093aa29a996235808980f12ba615347b72
Basant Bansal <itsbasanthere@gmail.com>
2026-04-04 10:56:36 +0530

    added something again

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://my-backend-6vy3.onrender.com",
   withCredentials: true
 });
 
